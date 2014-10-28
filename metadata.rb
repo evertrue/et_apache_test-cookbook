fb083caac78eab7115bc9489fdd3bf802f6b5b28
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-10-27 20:15:45 -0400

    Started work on Apache Module

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'et_apache_test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures et_apache_test'
+long_description 'Installs/Configures et_apache_test'
+version          '0.1.0'
+
