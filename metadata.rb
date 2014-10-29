5df9fb004dee0472281a9c9f71e42ea668424c00
Eddie Hurtig <eddie@hurtigtechnologies.com>
2014-10-29 02:32:13 -0400

    Reordered depends to the order recipe uses, added links

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,11 @@ description      'Installs/Configures et_apache_test'
 long_description 'Installs/Configures et_apache_test'
 version          '0.1.0'
 
-depends 'apache2', '~> 2.0.0'
-depends 'php', '~> 1.5.0'
+# https://github.com/opscode-cookbooks/apt
 depends 'apt', '~> 2.4'
+
+# https://github.com/opscode-cookbooks/php
+depends 'php', '~> 1.5.0'
+
+# https://github.com/viverae-cookbooks/apache2
+depends 'apache2', '~> 2.0.0'
