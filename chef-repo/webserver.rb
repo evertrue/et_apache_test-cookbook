d900cd18d8038dfca3472db3ae2098ede148e9d6
Ed <pratt.e@husky.neu.edu>
2015-12-02 11:42:48 -0800

    Remove my_app (a Berkshelf project) Add metadata file for chef Add beginning of chef cookbook

--- chef-repo/webserver.rb
+++ chef-repo/webserver.rb
@@ -1,16 +1,23 @@
 package 'apache2'
 
 
+#package 'apache2' do 
+#	package_name node['apache']['package']
+#end
+
+
 service 'apache2' do
 	supports :status => true
 	action [:enable, :start]
 end
 
+
 file 'index.html' do
 	content '<html>
 	<body>
-		<h1>Test</h1>
+		<h1>hello</h1>
 	</body>
 <html>'
 end
+
 
