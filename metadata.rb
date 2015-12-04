a933b62bb39348c4a5e18e325eccf53aa2b524e1
Eddie Pratt <epratt81@ccs.neu.edu>
2015-12-04 16:06:15 -0500

    Changed apache_config in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ depends 'apache2', '~> 3.1'
 
 # Disable the default virtual host using only attributes 
 # provided by the `apache2` cookbook (no recipe code)
-# apache_config 'a2ensite' do 
+
+# apache_config 'a2dissite' do 
 # 	enable false
 # end
 
