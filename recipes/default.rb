fdfb74c1c37100885bdd8c09d20e13709e2daad2
Eddie Pratt <epratt81@ccs.neu.edu>
2015-12-06 00:16:02 -0500

    Enable PHP5

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'apache2'
+include_recipe 'apache2::default'
+include_recipe 'apache2::mod_rewrite'
+include_recipe 'apache2::mod_deflate'
+include_recipe 'apache2::mod_headers'
 
 # disable default site
 apache_site '000-default' do
   enable false
+end
+
+apache_module "php5" do 
+	filename "libphp5.so"
 end
