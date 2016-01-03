e6c7f40777b7035bd247a56720e989003619ac94
Kyle Ryan <kyleryanj@gmail.com>
2016-01-03 05:23:11 -0500

    Add recipe code to create new web app directory and test file

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,3 +12,14 @@ include_recipe 'php'
 include_recipe 'apache2::default'
 include_recipe 'apache2::mod_php5' #need this otherwise php won't serve on apache
 
+directory "/srv/www/#{node['et_apache_test']['web_app']['name']}" do
+  action :create
+  recursive true #need this to create www in srv as well
+end
+
+cookbook_file "/srv/www/#{node['et_apache_test']['web_app']['name']}/index.php" do
+	source 'index.php'
+ 	mode '0755'
+  owner 'www-data'
+  group 'www-data'
+end
