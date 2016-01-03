2eb1ef2b3aaadaef7f464d6d09d77da9a62b4c53
Kyle Ryan <kyleryanj@gmail.com>
2016-01-03 04:35:24 -0500

    Initial cookbook generation for et_apache_test

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'et_apache_test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures et_apache_test'
+long_description 'Installs/Configures et_apache_test'
+version          '0.1.0'
