09d75d792aefcc48bf205780f7fa9d135d36c171
Kyle Ryan <kyleryanj@gmail.com>
2016-01-03 05:26:22 -0500

    Create new web app using web_app definition in apache2

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,3 +23,10 @@ cookbook_file "/srv/www/#{node['et_apache_test']['web_app']['name']}/index.php" 
   owner 'www-data'
   group 'www-data'
 end
+
+web_app node['et_apache_test']['web_app']['name'] do
+  server_name node['hostname']
+  server_aliases [node['fqdn'], node['et_apache_test']['web_app']['name']]
+  docroot "/srv/www/#{node['et_apache_test']['web_app']['name']}"
+  cookbook 'apache2'
+end
