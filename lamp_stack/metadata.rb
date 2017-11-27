356a081c2bf4a612b214d00a350413b14b19f4e3
omkumar0904 <32582661+omkumar0904@users.noreply.github.com>
2017-11-27 12:45:15 -0800

    Update metadata.rb

--- lamp_stack/metadata.rb
+++ lamp_stack/metadata.rb
@@ -5,7 +5,7 @@ license 'all_rights'
 description 'Wrapper Cookbook for lamp_stack Cookbook'
 long_description 'Wrapper Cookbook for lamp_stack Cookbook'
 issues_url 'https://projects.gci.com/jira'
-source_url 'http://source.gci.com/projects/CDCHEF/repos/gci_elk'
+source_url 'https://github.com/omkumar0904/et_apache_test-cookbook/edit/master/lamp_stack/'
 version '0.1.0'
 chef_version '>= 12' if respond_to?(:chef_version)
 
