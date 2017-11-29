6f719ac386e89cf1235a4d601267fcf3bf955926
manojkal <kmanoj.asu@gmail.com>
2017-11-29 02:55:06 +0000

    adding php and apache2 in recipe

--- lamp-stack/recipes/default.rb
+++ lamp-stack/recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'php'
+
+include_recipe 'apache2::default'
+include_recipe 'apache2::mod_php5' #need this otherwise php won't serve on apache
