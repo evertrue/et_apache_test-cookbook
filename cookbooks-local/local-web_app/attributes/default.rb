2f4adc4c5b22cbed8325e539abc1be7434854e84
Nico Kadel-Garcia <nkadel@gmail.com>
2017-12-15 15:36:46 -0500

    Teach local_web_app to use apache2 templates

--- cookbooks-local/local-web_app/attributes/default.rb
+++ cookbooks-local/local-web_app/attributes/default.rb
@@ -4,5 +4,17 @@
 #
 
 # Use system hostname by default
-default['local-web_app']['hostname'] = node['hostname']
+default['local-web_app']['virtualhost'] = node['hostname']
+#default['local-web_app']['serveradmin'] = node['hostname']
+default['local-web_app']['serveradmin'] = "webmaster@localhost"
+#default['local-web_app']['serveradmin'] = "webmaster@#{node['hostname']}"
+
+default['local-web_app']['servername'] = node['hostname']
+default['local-web_app']['serveraliases'] = [ "localhost", "localhost.localdomain" ]
+default['local-web_app']['docroot'] = node['apache2']['docroot_dir']
+#default['local-web_app']['docroot'] = "/var/www/#{node['local_web-app]]['virtualhost']}"
+
+
+
+
 
--- cookbooks-local/local-web_app/recipes/default.rb
+++ cookbooks-local/local-web_app/recipes/default.rb
@@ -5,6 +5,10 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 web_app "local-web_app" do
-  template "local-web_app.conf.erb"
-  server_name node['local-web_app']['hostname']
+  #template "local-web_app.conf.erb"
+  server_name node['local-web_app']['virtualhost']
+#  docroot node['local-web_app']['docroot']
+  docroot "/var/www/html"
+  cookbook "apache2"
+  notifies :reload, "service[apache2]", :delayed
 end
