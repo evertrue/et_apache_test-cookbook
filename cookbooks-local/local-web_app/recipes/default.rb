df3d966564883c906446801c8927153aad286b62
Nico Kadel-Garcia <nkadel@gmail.com>
2017-12-15 16:41:26 -0500

    Use apache2 default attributes more consistently, especially docroot

--- cookbooks-local/local-web_app/recipes/default.rb
+++ cookbooks-local/local-web_app/recipes/default.rb
@@ -2,13 +2,22 @@
 # Cookbook:: local-web_app
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Nico Kadel-Garcia, All Rights Reserved.
 
 web_app "local-web_app" do
-  #template "local-web_app.conf.erb"
   server_name node['local-web_app']['virtualhost']
-#  docroot node['local-web_app']['docroot']
-  docroot "/var/www/html"
+  server_aliases node['local-web_app']['server_aliases']
+  docroot node['local-web_app']['docroot_dir']
   cookbook "apache2"
-  notifies :reload, "service[apache2]", :delayed
+  notifies :reload, 'service[apache2]', :delayed
+end
+
+# Add to activate service, quited from apache2 cookbook
+apache_service_name = node['apache']['service_name']
+
+service 'apache2' do
+  service_name apache_service_name
+  supports [:start, :restart, :reload, :status]
+  action [:enable, :start]
+  only_if "#{node['apache']['binary']} -t", environment: { 'APACHE_LOG_DIR' => node['apache']['log_dir'] }, timeout: 10
 end
