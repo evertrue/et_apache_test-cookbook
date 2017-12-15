8fcfa6361576bbb95cc80c4ec72fb53456a99255
Nico Kadel-Garcia <nkadel@gmail.com>
2017-12-15 14:46:11 -0500

    Add recipes and empty teplates for local-web_app

---
+++ cookbooks-local/local-web_app/attributes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: local-web_app
+# Attributes:: default
+#
+
+# Use system hostname by default
+default['local-web_app']['hostname'] = node['hostname']
+
--- cookbooks-local/local-web_app/recipes/default.rb
+++ cookbooks-local/local-web_app/recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+web_app "local-web_app" do
+  template "local-web_app.conf.erb"
+  server_name node['local-web_app']['hostname']
+end
