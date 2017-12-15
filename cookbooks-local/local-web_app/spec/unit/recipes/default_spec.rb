e104d19faba31f438b86547ab997b4bc28b3a9fe
Nico Kadel-Garcia <nkadel@gmail.com>
2017-12-15 16:42:18 -0500

    Update more attributes

--- cookbooks-local/local-web_app/spec/unit/recipes/default_spec.rb
+++ cookbooks-local/local-web_app/spec/unit/recipes/default_spec.rb
@@ -2,7 +2,7 @@
 # Cookbook:: local-web_app
 # Spec:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Nico Kadel-Garcia, All Rights Reserved.
 
 require 'spec_helper'
 
